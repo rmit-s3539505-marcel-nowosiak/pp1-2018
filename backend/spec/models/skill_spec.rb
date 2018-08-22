b44f983e4d35ab3dd9b7c31e732a76e17bc4dc98
Rob <rlainewyllie@gmail.com>
2018-08-23 03:43:02 +1000

    Add more tests to skill_spec

--- backend/spec/models/skill_spec.rb
+++ backend/spec/models/skill_spec.rb
@@ -1,14 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  context "name" do
-    it "should be non-empty" do
+  context "with an name empty name" do
+    it "should be invalid" do
       s = Skill.new(:name => "")
       expect(s).to_not be_valid
     end
+  end
 
-    it "should not be nil" do
+  context "with a nil name" do
+    it "should not be valid" do
       s = Skill.new(:name => nil)
+      expect(s).to_not be_valid
+    end
+  end
+
+  context "with a name that has atleast 3 chars" do
+    it "should be valid" do
+      s = Skill.new(:name => 'Ski')
+      expect(s).to be_valid
+    end
+  end
+
+  context "with a name that has less than 3 chars" do
+    it "should not be valid" do
+      s = Skill.new(:name => 'ab')
+      expect(s).to_not be_valid
     end
   end
 end
