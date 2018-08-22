ae77e62251ed9b34268291bcbfc2adf793155632
Rob <rlainewyllie@gmail.com>
2018-08-23 01:51:12 +1000

    Generate devise model for hunter and employer

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  devise_for :employers, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_for :hunters, controllers: {
+    sessions: 'users/sessions'
+  }
+
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ backend/spec/factories/hunters.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :hunter do
+    
+  end
+end
