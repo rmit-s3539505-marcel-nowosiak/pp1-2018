e77aed644105a00436593e2d02bdc1fbfc536b0d
Rob <rlainewyllie@gmail.com>
2018-08-23 16:28:46 +1000

    Add nil variable declaration to user and profile

--- backend/app/controllers/dashboards_controller.rb
+++ backend/app/controllers/dashboards_controller.rb
@@ -1,7 +1,9 @@
 class DashboardsController < ApplicationController
   # GET /dashboards
   def index
+    # @user = nil
+    # @profile = nil
     @user = find_user(:session[:user_id])
-    @profile = find_hunter_profile(@user.id) if not @user.nil?
+    @profile = find_hunter_profile(@user.id) unless @user.nil?
   end
 end
