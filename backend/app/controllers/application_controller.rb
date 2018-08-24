f8b3906633c267e8d357cac1aee6d9dae670bbe8
liam ryan <s3606826@student.rmit.au>
2018-08-24 17:50:18 +1000

    set permitted params in application_controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_constroller?
   # before_action :require_login
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) {u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+  end
 end
