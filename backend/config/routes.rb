ddfdf40efadd900deb4d033a592903ece5f6c6f7
liam ryan <s3606826@student.rmit.au>
2018-08-24 17:48:32 +1000

    add root to routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, :controllers => { :passwords => "users/passwords", :confirmations => "users/confirmations" }
+  devise_scope :users do
+    get 'signup', to: 'users/registrations#new'
+    get 'signin', to: 'users/sessions#new'
+    delete 'signout', to: 'users/sessions#destroy'
+  end
 
   resources :listings
+  resources :registrations
+  resources :sessions
+
+  root "users/sessions#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
