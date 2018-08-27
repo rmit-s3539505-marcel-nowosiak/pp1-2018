7a39bffe50a1beff2a347c0d031bbbf263d477ad
liam ryan <s3606826@student.rmit.au>
2018-08-27 14:00:41 +1000

    give hunter many skill...

--- backend/app/models/hunter_profile.rb
+++ backend/app/models/hunter_profile.rb
@@ -1,5 +1,5 @@
 class HunterProfile < ApplicationRecord
   belongs_to :user
-  has_many :skills, :dependent => :destroy
+  has_many :skill, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
