ba00f0bf429891f259a94ed7168f0913c62802a5
Rob Laine <rlainewyllie@gmail.com>
2018-08-29 20:25:51 +1000

    Generate a usermailer mailer and add a call to send email to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
   #   # return true iff the employer table has a reference to this user's ID
   #   # true if Employer.find_by(:user_id => self.id) # psuedocode
   # end
+
+  # call the welcome_email method after a new user signs up
+  def after_confirmation
+    welcome_email
+  end
+
+  private
+  def welcome_email
+    UserMailer.with(user: @user).welcome_email.deliver_later
+  end
 end
