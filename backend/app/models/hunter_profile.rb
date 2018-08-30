5f3c0df33cae284720c402adc044a3a005b07975
liam ryan <s3606826@student.rmit.au>
2018-08-30 12:31:24 +1000

    remove condition for nested attribute

--- backend/app/models/hunter_profile.rb
+++ backend/app/models/hunter_profile.rb
@@ -1,5 +1,5 @@
 class HunterProfile < ApplicationRecord
-  belongs_to :user, :foreign_key => "user_id"
-  has_many :skill, :dependent => :destroy
-  accepts_nested_attributes_for :skill , :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  #belongs_to :user, :foreign_key => "user_id"
+  has_many :skill
+  accepts_nested_attributes_for :skill, :allow_destroy => true
 end
