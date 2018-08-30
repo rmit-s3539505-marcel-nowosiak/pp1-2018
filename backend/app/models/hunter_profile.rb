943ac1374664b05c486056fc175239e36ba7ced7
liam ryan <s3606826@student.rmit.au>
2018-08-30 13:38:45 +1000

    try to fix attribute error

--- backend/app/models/hunter_profile.rb
+++ backend/app/models/hunter_profile.rb
@@ -1,5 +1,6 @@
 class HunterProfile < ApplicationRecord
-  #belongs_to :user, :foreign_key => "user_id"
-  has_many :skill
+  belongs_to :user, :foreign_key => "user_id"
+  has_many :hunter_profiles_skills
+  has_many :skill, through :hunter_profiles_skills
   accepts_nested_attributes_for :skill, :allow_destroy => true
 end
