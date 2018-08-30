617c92eb9ad2379cf3e69cd001ea0167996d7fb5
Rob Laine <rlainewyllie@gmail.com>
2018-08-30 19:45:26 +1000

    Add listing worker
        - Add listings helper method to fetch listings and store in redis

--- backend/app/helpers/listings_helper.rb
+++ backend/app/helpers/listings_helper.rb
@@ -1,2 +1,10 @@
 module ListingsHelper
+  def fetch_listings_for_hunter(h_id)
+    listings = REDIS.get("listings")
+    if listings.nil?
+      listings = find_hunter_matches(h_id)
+      REDIS.set("listings", listings)
+    end
+    @listings = JSON.load listings
+  end
 end
