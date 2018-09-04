a284d38feb059d0423c876524c5c09bd0aa6a1b3
Rob Laine <rlainewyllie@gmail.com>
2018-09-04 20:10:35 +1000

    Implement methods to create a match from the attributes

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,11 +7,6 @@ class ApplicationController < ActionController::Base
   #   devise_parameter_sanitizer.for(:sign_up) {u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
   # end
 
-  # before_action :require_login
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-
   # define app wide helper methods to more easily render the dashboards
   def find_user(id)
     User.find(id)
@@ -34,22 +29,23 @@ class ApplicationController < ActionController::Base
     @matches = Hash.new
 
     # Return a hash of listings, with scores
-    @listings = Listing.where("
-        min_salary >= ? AND
-        hours == ?
-      ",
+    @listings = Listing.where("min_salary >= ?",
       # listings with a salary that satisfies the hunter
-      @hunter.min_salary,
-      # match job hunters who have preferred hours
-      @hunter.hours
+      @hunter.min_salary
     )
-    @listings.each do |l|
+
+    @listings.each do |listing|
       # generate and store the score of the match out of 100 to render in the view
       # as listings are only those which have matching hours and salary,
       # we aren't concerned with listings not in this set
-      score = 0
-      @matches[l] = score
+
+      # only store listings with a 50% or more match
+      percentage = percentage(score(@hunter, listing), max_score(listing))
+      if percentage > 50
+        @matches[listing] = percentage.round
+      end
     end
+    # return all the valid listings with their score
     return @matches
   end
 
@@ -58,27 +54,56 @@ class ApplicationController < ActionController::Base
     # Find the listing that has the particular ID
     @listing = Listing.find(listing_id)
     # find users whose preference for salary is less than the one offered
-    HunterProfile.where("min_salary < ?", @listing.min_salary)
+    HunterProfile.where("
+        min_salary < ?
+      ",
+      @listing.min_salary
+    )
   end
 
-  # return a float of the KM distance between loc1 and loc2, useful for match
+  # return a float of the metres distance between loc1 and loc2, useful for match
   # making based on distance. Should use google maps api
   def distance(loc1, loc2)
+    return 1 # TODO implement method
+  end
+
+  # return a score from a hunter profile and listing
+  def score(hunter, listing)
+    score = 0
+    # increment the score if there are matching hours
+    score += 25 if hunter.hours == listing.hours
+    # increment score if the locations are close
+    # TODO implement properly. Ie, if less than 1km, add 25,
+    #   if 1km < dist < 2km, add 20, etc
+    score += 25 if distance(hunter.location, listing.location) < 1000
+  end
+
+  # return the max_score available
+  def max_score(listing)
+    15 * listing.get_required_skills.count + # 15 for each skill
+    25 +  # hours
+    25    # location; Smaller the distance, higher the score
+  end
+
+  # return the percentage of the match
+  def percentage(score, max_score)
+    100 * (score.to_f / max_score.to_f)
   end
 
   private
-    def logged_in?
-      true unless current_user == nil
-    end
+  def logged_in?
+    true unless current_user == nil
+  end
 
-    def require_login
-      unless logged_in?
-        flash[:error] = "You must be logged in to access this section"
-        redirect_to new_user_session_url # halts request cycle
-      end
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user_session_url # halts request cycle
     end
+  end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+  end
+
 end
