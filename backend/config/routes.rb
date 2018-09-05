bb50bc9432896ec3189ccd245df27596f8ff24b7
liam ryan <s3606826@student.rmit.au>
2018-09-05 22:58:39 +1000

    add hunter_profile resource

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # resources :listings
   # resources :registrations
   # resources :sessions
+  resources :hunter_profiles
 
   root "dashboards#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
