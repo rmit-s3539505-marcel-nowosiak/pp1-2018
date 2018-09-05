4d41244df79c044d627a8ff5d56adcf6c30e7836
liam ryan <s3606826@student.rmit.au>
2018-09-05 22:56:57 +1000

    add jquery for salary slider

--- backend/app/assets/javascripts/application.js
+++ backend/app/assets/javascripts/application.js
@@ -14,4 +14,21 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require_jquery-ui
+
+$(document).ready(function() {
+    var slider = $("#money_slider").slider({
+        range: true,
+        min: 10000,
+        max: 500000,
+        values: [250000, 450000],
+        slide: function(event, ui) {
+            $("#hunter_profile_min_salary").val(ui.values[0]);
+            $("#hunter_profile_max_salary").val(ui.values[1]);
+        }
+    });
+    $("#hunter_profile_min_salary").val(slider.slider("values")[0]);
+    $("#hunter_profile_max_salary").val(slider.slider("values")[1]);
+});
+
 
