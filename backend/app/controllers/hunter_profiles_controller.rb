afcc8536fac1db0cf242d029960e5f04985e8276
liam <s3606826@student.rmit.au>
2018-09-16 12:59:22 +1000

    stuff and things...

--- backend/app/controllers/hunter_profiles_controller.rb
+++ backend/app/controllers/hunter_profiles_controller.rb
@@ -1,33 +1,66 @@
 class HunterProfilesController < ApplicationController
+  before_action :get_user, only: [:create, :edit, :update, :destroy]
+  before_action :get_profile, only: [:edit, :update, :destroy]
+  before_action :get_skills, only: [:new, :edit, :update]
 
   def index
-    @hunter_profile = HunterProfile.find(params[:id])
   end
 
   def show
-    @hunter_profile = HunterProfile.find(params[:id])
-    @user = @hunter_profile.user
   end
 
   def new
+    #@user.hunter_profile = HunterProfile.new
     @hunter_profile = HunterProfile.new
-    @skills = Skill.all.map{|u| { id: u.id, name: u.name, industry: u.industry}}
   end
 
   def create
+    @hunter_profile = HunterProfile.new(profile_params)
+    #@hunter_profile.user_id = current_user.id
+    if @hunter_profile.save
+      @user.hunter_profile = @hunter_profile
+      flash[:notice] = ["Your profile was successfully created"]
+    else
+      flash[:notice] = ["Your profile could not be created"]
+    end
+    redirect_to root_url
   end
 
   def edit
-    @hunter_profile = HunterProfile.find(params[:id])
   end
 
   def update
+    if @hunter_profile.update(profile_params)
+      flash[:notice] = ["Your profile was successfully updated"]
+      render 'show'
+    else
+      flash[:notice] = ["Your profile could not be updated"]
+      redirect_to root_url
+    end
   end
 
   def destroy
-    @hunter_profile = hunter_profile.find(params[:id])
     @hunter_profile.destroy
     flash[:notice] = "Successfully destroyed job hunter profile."
     redirect_to hunter_profiles_url
   end
+
+  private
+
+    def get_skills
+      @skills = Skill.all.map{|u| { id: u.id, name: u.name, industry: u.industry}}
+    end
+
+    def get_profile
+      @hunter_profile = @user.hunter_profile
+    end
+
+    def get_user
+      @user = current_user
+      #@user = User.find(params[:id])
+    end
+
+    def profile_params
+      params.permit(:industry, :hours, :location, :min_salary, :max_salary, :skill1, :skill2, :skill3, :skill4, :skill5)
+    end
 end
