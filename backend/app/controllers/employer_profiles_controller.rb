d6e2db438a06c0e983e61b2abe23f0997febc17e
liam <s3606826@student.rmit.au>
2018-09-20 07:05:04 +1000

    make new/create/edit/update/delete for employer profile

--- backend/app/controllers/employer_profiles_controller.rb
+++ backend/app/controllers/employer_profiles_controller.rb
@@ -1,4 +1,74 @@
 class EmployerProfilesController < ApplicationController
-  def new
+  before_action :get_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :get_profile, only: [:edit, :update, :destroy]
+
+  def index
   end
+
+  def show
+  end
+
+  def new
+    if @user.employer_profile.present?
+      puts "profile exist"
+      redirect_to edit_employer_profile_path(@user.id)
+    else
+      @employer_profile = EmployerProfile.new
+      puts "no profile, creating"
+    end
+  end
+
+  def create
+    @employer_profile = EmployerProfile.new(profile_params)
+    @employer_profile.user_id = current_user.id
+    if @employer_profile.save
+      @user.employer_profile = @employer_profile
+      flash[:notice] = ["Your profile was successfully created"]
+      puts "Your profile was successfully created"
+      redirect_to edit_employer_profile_path(@user.id)
+    else
+      flash[:notice] = ["Your profile could not be created"]
+      puts "Your profile was not created"
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    if @user.employer_profile.present?
+      puts "profile exist"
+    else
+      puts "profile doesnt exist"
+      redirect_to new_employer_profile_path
+    end
+  end
+
+  def update
+    if @employer_profile.update(profile_params)
+      puts "Your profile was successfully updated"
+      flash[:notice] = ["Your profile was successfully updated"]
+    else
+      flash[:notice] = ["Your profile could not be updated"]
+    end
+  end
+
+  def destroy
+    @user.employer_profile.destroy
+    flash[:notice] = "Successfully destroyed job hunter profile."
+    redirect_to edit_employer_profile_path(@user.id)
+    puts "Your profile was successfully deleted"
+  end
+
+  private
+
+    def get_profile
+      @employer_profile = @user.employer_profile
+    end
+
+    def get_user
+      @user = current_user
+    end
+
+    def profile_params
+      params[:employer_profile].permit(:location, :business_name)
+    end
 end
