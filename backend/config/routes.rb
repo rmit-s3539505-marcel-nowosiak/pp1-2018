5ca6e5909458bde7583205f7c1e32e17cd79dfe3
liam <s3606826@student.rmit.au>
2018-09-20 10:02:51 +1000

    add employer_profile resource to routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # resources :registrations
   # resources :sessions
   resources :hunter_profiles
+  resources :employer_profiles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
