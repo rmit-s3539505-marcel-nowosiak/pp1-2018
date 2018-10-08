4f5b9576ce0c74b236ff5340152bbb4202c5e487
Rob Laine <rlainewyllie@gmail.com>
2018-10-08 16:42:41 +1100

    Add unit test for emp profile

--- backend/spec/models/employer_profile_spec.rb
+++ backend/spec/models/employer_profile_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe EmployerProfile, type: :model do
+  fixtures :employer_profiles, :listings, :users
   context "with 2 or more listings" do
-    it "is able to retrieve all of them" do
-      # e = EmployerProfile
-      # e.listings << Listing
+    it "should be able to retrieve all of them" do
+      e = employer_profiles(:e1)
+      e.listings << [listings(:l1), listings(:l2)]
+      expect(e.listings.count).to eq 2
+    end
+  end
+
+  context "attached to a user" do
+    it "should be able to retrieve the user" do
+      e = employer_profiles(:e1)
+      e.user = users(:u1)
+      expect(e.user).to eq users(:u1)
     end
   end
 end
