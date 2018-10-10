121e152abe90a36f5bd383356d452839e32f2191
Rob Laine <rlainewyllie@gmail.com>
2018-10-10 15:36:28 +1100

    Add more specs for models

--- backend/spec/models/employer_profile_spec.rb
+++ backend/spec/models/employer_profile_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe EmployerProfile, type: :model do
   fixtures :employer_profiles, :listings, :users
+  context "newly created" do
+    it "should have no listings to begin with" do
+      expect(EmployerProfile.new.listings.count).to eq 0
+    end
+
+    it "should not be attached to a user" do
+      expect(EmployerProfile.new.user).to eq nil
+    end
+  end
+
   context "with 2 or more listings" do
     it "should be able to retrieve all of them" do
       e = employer_profiles(:e1)
@@ -15,6 +25,12 @@ RSpec.describe EmployerProfile, type: :model do
       e = employer_profiles(:e1)
       e.user = users(:u1)
       expect(e.user).to eq users(:u1)
+    end
+
+    it "should be able to retrieve user's name" do
+      e = employer_profiles(:e1)
+      e.user = users(:u1)
+      exect(e.user.name).to eq users(:u1).first_name
     end
   end
 end
