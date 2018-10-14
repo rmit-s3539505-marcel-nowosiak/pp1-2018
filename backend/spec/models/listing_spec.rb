b25b96db1cdac41ac0bafcf0f6e0f72fa247c3bb
Rob Laine <rlainewyllie@gmail.com>
2018-10-15 03:14:48 +1100

    Remove redundant tests

--- backend/spec/models/listing_spec.rb
+++ backend/spec/models/listing_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  
+  fixtures :listings, :hunter_profiles, :employer_profiles
+  context "newly created" do
+    it "should have no employer profile attached" do
+
+    end
+  end
 end
