d27b6b9ed38bf27b2e257d934c220bddd0216e09
Rob Laine <rlainewyllie@gmail.com>
2018-10-19 01:38:12 +1100

    add nil check

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@' + ENV['APP_NAME'] + '.com'
+  default from: 'notifications@' + ENV['APP_NAME'].to_s + '.com' unless ENV['APP_NAME'].nil?
 
   def welcome_email
-    @user = params[:user]
-    @url  = 'http://' + ENV['APP_NAME'] + '.com/login'
-    mail(to: @user.email, subject: 'Welcome to HireMe!')
+    @user = params[:user] 
+    @url  = 'http://' + ENV['APP_NAME'].to_s + '.com/login' unless ENV['APP_NAME'].nil?
+    mail(to: @user.email, subject: 'Welcome to HireMe!') unless @user.nil?
   end
 end
