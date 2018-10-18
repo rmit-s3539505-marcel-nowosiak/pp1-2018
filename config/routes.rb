5c8598fade972983c64fd7dd1b8596de8ffc8f85
Rob Laine <rlainewyllie@gmail.com>
2018-10-19 01:45:19 +1100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
+  # get 'hunter_profiles/new'
+  root 'dashboards#index'
+
+  resources :hunter_profiles do
+    member do
+      get 'offer'
+      post 'offer'
+    end
+  end
+
+  devise_for :users, :controllers => {
+    :registrations => "users/registrations",
+    :sessions => "users/sessions",
+    :passwords => "users/passwords",
+    :confirmations => "users/confirmations"
+  }
+  devise_scope :users do
+    get 'sign_up', to: 'users/registrations#new'
+    get 'sign_in', to: 'users/sessions#new'
+    delete 'sign_out', to: 'users/sessions#destroy'
+  end
+
+  # post 'offer', to: 'hunter_profiles#create_offer'
+
+  resources :listings
+  # resources :registrations
+  # resources :sessions
+  resources :hunter_profiles
+  resources :employer_profiles
+
+  # create new employments from the offer button
+  resources :employments, only: [:new, :create]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
